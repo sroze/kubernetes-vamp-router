9a2269aec9604dc8e467c357522c4d2edeb23562
Samuel ROZE <samuel.roze@gmail.com>
2015-11-11 11:12:50 +0000

    Strip route name with MD5 when possible

--- routes.go
+++ routes.go
@@ -2,15 +2,24 @@ package main
 
 import (
 	"strings"
+	"crypto/md5"
+	"encoding/hex"
 
 	"k8s.io/kubernetes/pkg/api"
 )
 
 func (su *ServiceUpdater) GetServiceRouteName(service *api.Service) string {
-	return strings.Join([]string{
+	name := strings.Join([]string{
 		service.ObjectMeta.Name,
 		service.ObjectMeta.Namespace,
 	}, ".")
+
+	if len(name) > 63 {
+		nameHash := GetMD5Hash(name)[0:10]
+		name = name[0:52]+"-"+nameHash
+	}
+
+	return name
 }
 
 func (su *ServiceUpdater) GetDomainNameFromService(service *api.Service) string {
@@ -18,4 +27,9 @@ func (su *ServiceUpdater) GetDomainNameFromService(service *api.Service) string 
 		su.GetServiceRouteName(service),
 		su.Configuration.RootDns,
 	}, ".")
+}
+
+func GetMD5Hash(text string) string {
+	hash := md5.Sum([]byte(text))
+	return hex.EncodeToString(hash[:])
 }
